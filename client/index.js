e3d47f97c2672e64a749392bb665bc9de3c8b1c1
Andrew Wiggin <andrew@wiggin.dev>
2023-06-04 11:36:37 -0400

    Init warn-on package

---
+++ client/index.js
@@ -0,0 +1,4 @@
+console.warn(
+    "This module cannot be imported from a Client Component module. " +
+    "It should only be used from a Server Component."
+);
---
+++ server/warn.js
@@ -0,0 +1,4 @@
+console.warn(
+    "This module cannot be imported from a Server Component module. " +
+    "It should only be used from a Client Component."
+);
